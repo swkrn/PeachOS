4e286a0669195f691e48be129d923a1a4572723e
mon <skmon43@yahoo.com>
2025-10-10 17:15:23 +0700

    improve bootloader

--- boot.asm
+++ boot.asm
@@ -1,8 +1,39 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
 start:
-    mov si, message
+    jmp 0x7c0:step2
+
+step2:
+    cli                 ; clear interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti                 ; enables interrupts
+
+    mov ah, 2           ; READ SECTOR COMMAND
+    mov al, 1           ; ONE SECTOR TO READ
+    mov cl, 0           ; Cylinder low eight bits
+    mov cl, 2           ; Read sector two
+    mov dh, 0           ; Head number
+    mov bx, buffer
+    int 0x13
+    jc error
+
+    mov si, buffer
+    call print
+    jmp $
+
+error:
+    mov si, error_message
     call print
     jmp $
 
@@ -22,6 +53,9 @@ print_char:
     int 0x10
     ret
 
-message: db 'Hello World', 0
+error_message: db 'Failed to load sector', 0
+
 times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer:
