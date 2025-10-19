b3630a1e37fdf896b0a782ef6a16cf20a094bcc9
mon <skmon43@yahoo.com>
2025-10-19 15:05:24 +0700

    heap & memory allocation

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,7 @@
 #include "kernel.h"
 #include "idt/idt.h"
 #include "io/io.h"
+#include "memory/heap/kheap.h"
 
 uint16_t *video_mem = 0;
 
@@ -67,6 +68,20 @@ void kernel_main()
     terminal_initialize();
     print("Suck you\nducker!");
 
+    // Initialize the heap
+    kheap_init();
+
     // Initialize the interrupt descriptor table
     idt_init();
+
+    // TODO: remove test
+    void *ptr = kmalloc(50);
+    void *ptr2 = kmalloc(5000);
+    void *ptr3 = kmalloc(5600);
+
+    kfree(ptr);
+
+    void *ptr4 = kmalloc(50);
+
+    if (ptr || ptr2 || ptr3 || ptr4);
 }
