bf12af865c7966129ef10526907074a5f6647e41
mon <skmon43@yahoo.com>
2025-11-02 16:15:21 +0700

    vfs core

--- src/disk/disk.h
+++ src/disk/disk.h
@@ -1,6 +1,8 @@
 #ifndef DISK_H
 #define DISK_H
 
+#include "fs/file.h"
+
 typedef unsigned int PEACHOS_DISK_TYPE;
 
 // Represents a real physical hard disk
@@ -10,6 +12,8 @@ struct disk
 {
     PEACHOS_DISK_TYPE type;
     int sector_size;
+
+    struct filesystem *filesystem;
 };
 
 void disk_search_and_init();
