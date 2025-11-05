17c26829284c458314961377e57fa47130391eec
mon <skmon43@yahoo.com>
2025-11-05 09:19:23 +0700

    vfs fopen

--- src/string/string.c
+++ src/string/string.c
@@ -1,5 +1,14 @@
 #include "string.h"
 
+char tolower(char c)
+{
+    if (c >= 65 && c <= 90)
+    {
+        c += 32;
+    }
+    return c;
+}
+
 size_t strlen(const char *str)
 {
     size_t len = 0;
@@ -17,6 +26,50 @@ size_t strnlen(const char *str, size_t max)
             return max;
     };
     return len;
+}
+
+int strnlen_terminator(const char *str, size_t max, char terminator)
+{
+    size_t len = 0;
+    for (; len < max; len++)
+    {
+        if (!str[len])
+            return len;
+
+        if (str[len] == terminator)
+            return len;
+    }
+    return len;
+}
+
+int istrncmp(const char *s1, const char *s2, size_t size)
+{
+    while (size--)
+    {
+        unsigned char c1 = *s1++;
+        unsigned char c2 = *s2++;
+        if (c1 != c2 && tolower(c1) != tolower(c2))
+            return c1 - c2;
+
+        if (c1 == '\0')
+            return 0;
+    }
+    return 0;
+}
+
+int strncmp(const char *s1, const char *s2, size_t size)
+{
+    while (size--)
+    {
+        unsigned char c1 = *s1++;
+        unsigned char c2 = *s2++;
+        if (c1 != c2)
+            return c1 - c2;
+
+        if (c1 == '\0')
+            return 0;
+    }
+    return 0;
 }
 
 char* strcpy(char *dest, const char *source)
--- src/string/string.h
+++ src/string/string.h
@@ -7,7 +7,10 @@
 
 size_t strlen(const char *ptr);
 size_t strnlen(const char *str, size_t max);
+int strnlen_terminator(const char *str, size_t max, char terminator);
 char* strcpy(char *dest, const char *source);
+int istrncmp(const char *s1, const char *s2, size_t size);
+int strncmp(const char *s1, const char *s2, size_t size);
 bool isdigit(char c);
 int tonumericdigit(char c);
 
