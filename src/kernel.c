fd7d14122159263cb6909323f734b79e2d113499
mon <skmon43@yahoo.com>
2025-11-23 11:30:30 +0700

    panic

--- src/kernel.c
+++ src/kernel.c
@@ -63,6 +63,13 @@ void print(const char *str)
 }
 
 struct paging_4gb_chunk *kernel_chunk = 0;
+
+void panic(const char *msg)
+{
+    print(msg);
+    while (1);
+}
+
 void kernel_main()
 {
     terminal_initialize();
