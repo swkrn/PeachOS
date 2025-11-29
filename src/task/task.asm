a2eb8d91d493df62bdb1fe614e330c09168056a9
mon <skmon43@yahoo.com>
2025-11-29 09:15:19 +0700

    user land funtionality

---
+++ src/task/task.asm
@@ -0,0 +1,74 @@
+[BITS 32]
+section .asm
+
+global restore_general_purpose_registers
+global task_return
+global user_registers
+
+; void task_return(sturct registers *regs)
+task_return:
+    mov ebp, esp
+    ; Push the data segment (SS will be fine)
+    ; Push the stack address
+    ; Push the flags
+    ; Push the code segment
+    ; Push IP
+
+    ; Access the structure passed to this function
+    mov ebx, [ebp + 4]
+    ; push the data/stack selector
+    push dword [ebx + 44]
+    ; push the stack pointer
+    push dword [ebx + 40]
+
+    ; push the flags
+    pushf
+    pop eax
+    or eax, 0x200
+    push eax
+
+    ; push the code segment
+    push dword [ebx + 32]
+
+    ; push the IP to execute
+    push dword [ebx + 28]
+
+    ; setup some segment registers
+    mov ax, [ebx + 44]
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    push dword [ebx + 4]
+    call restore_general_purpose_registers
+    add esp, 4
+
+    ; leave kernel land and execute in user land!
+    iretd
+
+
+; void restore_general_purpose_registers(sturct registers *regs)
+restore_general_purpose_registers:
+    push ebp
+    mov ebp, esp
+    mov ebx, [ebp + 8]
+    mov edi, [ebx]
+    mov esi, [ebx + 4]
+    mov ebp, [ebx + 8]
+    mov edx, [ebx + 16]
+    mov ecx, [ebx + 20]
+    mov eax, [ebx + 24]
+    mov ebx, [ebx + 12]
+    pop ebp
+    ret
+
+
+; void user_registers()
+user_registers:
+    mov ax, 0x23
+    mov ds, ax
+    mov es, ax
+    mov fs, ax 
+    mov gs, ax
+    ret
