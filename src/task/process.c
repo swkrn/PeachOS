9acc070558d6a2972e857f27b6a87ff531f7c62c
mon <skmon43@yahoo.com>
2026-02-13 13:24:16 +0700

    refactor paging

--- src/task/process.c
+++ src/task/process.c
@@ -82,7 +82,7 @@ int process_map_binary(struct process *process)
 {
     int res = 0;
     paging_map_to(
-        process->task->page_directory->directory_entry, 
+        process->task->page_directory, 
         (void *)PEACHOS_PROGRAM_VIRTUAL_ADDRESS, 
         process->ptr, 
         paging_align_address(process->ptr + process->size), 
